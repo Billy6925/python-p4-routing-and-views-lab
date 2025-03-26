63233af1b2aad5a4c606c83f6e70acd4cc1e7c2e
Billy Eskein <billyeskein@gmail.com>
2025-03-26 15:53:10 +0300

    Update server/app.py

--- server/app.py
+++ server/app.py
@@ -1,8 +1,39 @@
-#!/usr/bin/env python3
-
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
+# Index route
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+# Print route
+@app.route('/print/<parameter>')
+def print_parameter(parameter):
+    print(parameter)  # Print to console
+    return parameter  # Display in browser
+
+# Count route
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    return '\n'.join(str(i) for i in range(parameter)) + '\n'
+
+# Math route
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        return 'Invalid operation'
+    return str(result)
+
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(debug=True)
